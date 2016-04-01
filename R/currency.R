99fc0d3bc2fbc8fc491c58ae7846e910f34c1e25
Peter Hurford <peter@peterhurford.com>
2016-04-01 00:52:32 -0500

    Initial currency function signatures

---
+++ R/currency.R
@@ -0,0 +1,11 @@
+currency <- ensure(
+  pre = list(
+    amount %is% numeric, length(amount) == 1,
+    from %in% currency_codes(),
+    to %in% currency_codes()),
+  post = list(
+    result %is% currency
+  ),
+  function(amount, from = "USD", to = "USD") {
+    "TODO"
+  })
