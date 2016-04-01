66232e583815553ae00449ecd5fa8b503b1236fa
Peter Hurford <peter@peterhurford.com>
2016-04-01 03:34:14 -0500

    Add more currencies; get_code_from_unit; simplify currency listing

--- R/currency_codes.R
+++ R/currency_codes.R
@@ -1,16 +1,41 @@
 currency_map <- memoise::memoise(function() {
   list(
-    list("USD", "American Dollar"),
-    list("CAD", "Canadian Dollar"),
-    list("EUR", "Euro")
+    "AUD" = "Australian Dollar",
+    "CAD" = "Canadian Dollar",
+    "CHF" = "Swiss Franc",
+    "CNY" = "Chinese Yuan",
+    "CZK" = "Czech Crown",
+    "DKK" = "",
+    "EUR" = "Euro",
+    "GBP" = "British Pound",
+    "HRK" = "",
+    "HUF" = "",
+    "INR" = "",
+    "JPY" = "",
+    "MEX" = "",
+    "MYR" = "Malaysian Ringgit",
+    "NZD" = "New Zealand Dollars",
+    "NOK" = "Norwegian Krone",
+    "PLN" = "",
+    "BRL" = "Brazillian Real",
+    "RUB" = "",
+    "SEK" = "",
+    "SGD" = "Singapore Dollar",
+    "TRY" = "Turkish Lira",
+    "USD" = "American Dollar",
+    "ZAR" = "South African Rands"
   )})
 
 currency_codes <- memoise::memoise(
   checkr::ensure(post = list(result %is% vector, result %contains_only% simple_string),
   function() {
-    unlist(lapply(currency_map(), `[[`, 1))
+    unlist(names(currency_map()))
   }))
 
 get_unit_from_code <- function(code) {
-  Find(function(x) identical(x[[1]], code), currency_map())[[2]]
+  currency_map()[[code]]
+}
+
+get_code_from_unit <- function(unit) {
+  names(currency_map())[grep(tolower(unit), lapply(currency_map(), tolower))]
 }
